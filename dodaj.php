190c6d85838567b1de5f1be8f943af03c4da409d
Maciej Molenda <maciekm777@gmail.com>
2017-07-03 14:13:16 +0200

    Create dodaj.php

---
+++ dodaj.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+<title>Ankieta - dodano głos</title>
+<meta charset="utf-8">
+<link href="style.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+
+<div class="container">
+	
+<?php
+$odp=$_POST["odp"];
+if ($odp == "A") { echo "<h2>Twoja ulubiona herbata to czarna</h2>";} 
+elseif ($odp == "B") { echo "<h2>Twoja ulubiona herbata to Earl Grey</h2>"; } 
+elseif ($odp == "C") { echo "<h2>Twoja ulubiona herbata to zielona</h2>";} 
+elseif ($odp == "D") { echo "<h2>Twoja ulubiona herbata to szałwiowa</h2>";} 
+elseif ($odp == "E") { echo "<h2>Twoja ulubiona herbata to mietowa</h2>";} 
+elseif ($odp == "F") { echo "<h2>Twoja ulubiona herbata to melisowa</h2>";} 
+elseif ($odp == "G") { echo "<h2>Twoja ulubiona herbata to rumiankowa</h2>";} 
+elseif ($odp == "H") { echo "<h2>Twoja ulubiona herbata to dziurawcowa</h2>";} 
+elseif ($odp == "I") { echo "<h2>Twoja ulubiona herbata to jaśminowa</h2>";}
+else { echo "<h2>Nie wybrano żadnej herbaty</h2>"; }	
+
+mysql_close($polaczenie);
+?>
+
+<h2>Dziękujemy za wypełnienie ankiety</h2>
+</div>
+</body>
+</html>	
